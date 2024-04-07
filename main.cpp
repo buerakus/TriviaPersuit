ff01559519d32fbfedbd183aeb80081ac8f26716
buerakus <112691067+buerakus@users.noreply.github.com>
2024-04-07 21:53:25 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,22 +13,33 @@ void static registration(string username, string password) {
 	}
 	*/
 	ofstream credentials("credentials.txt", std::ios::app);
-	credentials << username << ", " << password << "\n";
+	credentials << username << "," << password << "\n";
 	credentials.close();
-	cout << "Successfully registered as student!";
+	cout << "Successfully registered as student!" << endl;
 }
 
 void static adminlogin(string username, string password) {
-	if (username == "admin",password == "admin") {
-		cout << "Welcome admin!";
+	if (username == "admin" && password == "admin") {
+		cout << "Welcome admin!" << endl;
 	}
 }
 
-void static studentlogin(string username, string password) {
+int static studentlogin(string username, string password) {
 	fstream credentials("credentials.txt", ios::in);
+	bool found = false;
 	if (credentials.is_open()) {
-		string sa;
-		while (getline(credentials, sa, ', '));
+		string u;
+		string p;
+		while (getline(credentials, u, ',') && getline(credentials, p)) {
+			if (username == u) {
+				found = true;
+				if (password == p) {
+					cout << "Welcome " << username << endl;
+					credentials.close();
+					return 0;
+				}
+			}
+		}
 	}
 }
 
@@ -39,30 +50,34 @@ int main() {
 		string username;
 		string password;
 		cout << "Welcome to the Trivia Persuit DSTR Ed.\n";
-		cout << "Choose option :\n 1. Login as Student\n2. Login as Teacher\n3. Register as student\n4. Leave\n";
+		cout << "Choose option :\n1. Login as Student\n2. Login as Teacher\n3. Register as student\n4. Leave\n";
 		cin >> choice;
 		switch (choice) {
 		case 1:
-			cout << "Enter login";
+			cout << "Enter login : " << endl;
 			cin >> username;
-			cout << "Enter password";
+			cout << "Enter password : " << endl;
 			cin >> password;
 			studentlogin(username, password);
+			break;
 		case 2:
-			cout << "Enter login";
+			cout << "Enter login : " << endl;
 			cin >> username;
-			cout << "Enter password";
+			cout << "Enter password : " << endl;
 			cin >> password;
 			adminlogin(username, password);
+			break;
 		case 3:
-			cout << "Enter login\n";
+			cout << "Enter login : " << endl;
 			cin >> username;
-			cout << "Enter password\n";
+			cout << "Enter password : " << endl;
 			cin >> password;
 			registration(username, password);
+			break;
 		case 4:
 			cout << "Goodbye!";
 			x++;
+			break;
 		}
 	}
 }
