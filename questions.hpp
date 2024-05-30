7d83e1ed7d45d6828d5c14726f53e58d83612019
Timur J <112691067+messershmid@users.noreply.github.com>
2024-05-31 01:04:29 +0800

    Update questions.hpp

--- questions.hpp
+++ questions.hpp
@@ -1,7 +1,9 @@
 #ifndef QUESTIONS_HPP
 #define QUESTIONS_HPP
 
+#include <iostream>
 #include <string>
+#include <stack>
 
 class Question {
 private:
@@ -16,6 +18,26 @@ public:
 
     int getScore() const { return score; }
     void setScore(int s) { score = s; }
+};
+
+class QuestionStack {
+private:
+    std::stack<Question> stack;
+
+public:
+    void push(const Question& question) {
+        stack.push(question);
+    }
+
+    Question pop() {
+        Question top = stack.top();
+        stack.pop();
+        return top;
+    }
+
+    bool isEmpty() const {
+        return stack.empty();
+    }
 };
 
 #endif // QUESTIONS_HPP
